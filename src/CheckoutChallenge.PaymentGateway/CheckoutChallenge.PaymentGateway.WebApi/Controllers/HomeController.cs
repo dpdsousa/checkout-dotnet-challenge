c9a7f13b51433dfaec1c6fc2490ca5c8ca876e0f
Diogo Sousa <60160638+dpdsousa@users.noreply.github.com>
2020-06-16 12:24:27 +0100

    Added webapi starting project

---
+++ src/CheckoutChallenge.PaymentGateway/CheckoutChallenge.PaymentGateway.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CheckoutChallenge.PaymentGateway.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Up and Running!");
+        }
+    }
+}
