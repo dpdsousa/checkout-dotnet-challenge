791834702ff03ad9b048fda64af5b8775c2f22c4
Diogo Sousa <60160638+dpdsousa@users.noreply.github.com>
2020-06-22 12:03:26 +0100

    Modified docker to run the two API containers

--- src/CheckoutChallenge.BankServiceMock/Core/AppStatus.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CheckoutChallenge.BankServiceMock.Core
-{
-    public class AppStatus
-    {
-        public string Status { get; set; }
-        public string Message { get; set; }
-    }
-}
